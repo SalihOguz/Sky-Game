21dfc90a00df4367740c240cf5a5b6a6bc0f25a6
Mustafa Salih Oğuz <mustafasalihoguz@gmail.com>
2020-08-09 19:01:23 +0300

    Add easy camera change for demo

---
+++ Assets/Scripts/Cameracontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontroller : MonoBehaviour
+{
+    [SerializeField] private GameObject fpCam;
+    [SerializeField] private GameObject tpCam;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            fpCam.SetActive(true);
+            tpCam.SetActive(false);
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            fpCam.SetActive(false);
+            tpCam.SetActive(true);
+        }
+    }
+}
